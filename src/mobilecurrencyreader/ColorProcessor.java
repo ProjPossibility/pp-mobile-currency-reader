350d8bec05cf5a5fd3d97ec64cf1ae8b9cb641eb
mcrdrteam <mcrdrteam@d79de2aa-6670-11de-873c-fb8c264d332a>
2007-11-17 21:12:35 +0000

    color processing interface

---
+++ src/mobilecurrencyreader/ColorProcessor.java
@@ -0,0 +1,21 @@
+/*
+ * GeometryProcessor.java
+ *
+ * Created on November 17, 2007, 12:53 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package mobilecurrencyreader;
+
+/**
+ *
+ * @author binarygame
+ */
+public interface ColorProcessor {
+    public byte[] convertToGrayscale(byte original[], int height, int width);
+    public byte[] expandDynamicRange(byte original[], int height, int width);
+    public byte[] quanize(byte original[], int height, int width, int N);
+    public double subtractImage(byte a[], byte b[], int height, int width);
+}
